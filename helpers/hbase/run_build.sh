4a31267697c4211bd5e069a80ee3a20e44731159
kaushalkahapola <kaushal.21@cse.mrt.ac.lk>
2026-01-09 14:28:56 +0530

    fix: correct Docker build context path in run_build.sh

--- helpers/hbase/run_build.sh
+++ helpers/hbase/run_build.sh
@@ -8,7 +8,7 @@ echo "--- Building code for ${COMMIT_SHA:0:7} ---"
 docker volume create maven-cache-hbase 2>/dev/null || true
 
 echo "--- Building Docker image... ---"
-docker build -t ${IMAGE_TAG} -f ${TOOLKIT_DIR}/Dockerfile .
+docker build -t ${IMAGE_TAG} -f ${TOOLKIT_DIR}/Dockerfile ${TOOLKIT_DIR}
 
 echo "--- Preparing build directory... ---"
 # Copy source code to BUILD_DIR
